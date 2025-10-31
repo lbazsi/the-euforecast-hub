6ed434dd75c0e97ab52aa1797b7842a55e5e05db
Balazs Laszlo <lbazsi09@gmail.com>
2025-10-31 15:12:42 +0100

    Update forecasting.py

--- app/models/forecasting.py
+++ app/models/forecasting.py
@@ -1,1 +1,9 @@
+import numpy as np
+import pandas as pd
 
+def simple_forecast(n=36):
+    """Simple linear forecast for demo."""
+    t = np.arange(n)
+    values = 100 + 0.5 * t + np.random.normal(0, 1, n)
+    df = pd.DataFrame({"time": list(range(n)), "values": values})
+    return df
